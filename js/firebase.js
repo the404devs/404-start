be13dd6ee92a8c8e56997efc0d7976aa2ec211ad
Owen Goodwin <the404devs@gmail.com>
2020-05-08 12:09:44 -0400

    0.1.4: less hardcoded values, allowing for easier customization

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,25 @@
 // Initialize Firebase
+/* CHANGE THE INFO HERE TO MATCH YOUR OWN DATABASE
+	Otherwise, you'll be seeing my own to-do list. */
 var config = {
-	apiKey: "AIzaSyBP6KLfzsYrYfMb66aQKjPaupm_JJT2uHI",
-	authDomain: "homepage-events.firebaseapp.com",
-	databaseURL: "https://homepage-events.firebaseio.com",
-	projectId: "homepage-events",
-	storageBucket: "",
-	messagingSenderId: "111058101137"
+    apiKey: "AIzaSyBP6KLfzsYrYfMb66aQKjPaupm_JJT2uHI",
+    authDomain: "homepage-events.firebaseapp.com",
+    databaseURL: "https://homepage-events.firebaseio.com",
+    projectId: "homepage-events",
+    storageBucket: "",
+    messagingSenderId: "111058101137"
 };
+
+
 firebase.initializeApp(config);
-$(function(){
-  readFromFire();
-  $('.delete').click(deleteEvent);
+$(function() {
+    readFromFire();
+    $('.delete').click(deleteEvent);
 });
 
-this.deleteEvent = function(e){
-	var box = $(e.target);
-	console.log(box.closest('.eventTitle').html());
+this.deleteEvent = function(e) {
+    var box = $(e.target);
+    console.log(box.closest('.eventTitle').html());
 }.bind(this);
 
 
@@ -23,62 +27,62 @@ var db = firebase.database();
 var events = db.ref("events");
 var eventsList;
 events.on("value", function(snapshot) {
-  eventsList = (snapshot.val());
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
+    eventsList = (snapshot.val());
+}, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
 });
 
-function writeToFire(){
-	var title = $('#eventName').val();
-	var date = $('#eventDate').val();
-	var time = $('#eventTime').val();
-	var desc = $('#eventBody').val();
-	events.child(date+"-"+time+"-"+title).set({
-		title: title,
-		date: date,
-		time: time,
-		description: desc,
-	});
-	readFromFire();
-	$('#eventName').val("");
-	$('#eventDate').val("");
-	$('#eventTime').val("");
-	$('#eventBody').val("");
+function writeToFire() {
+    var title = $('#eventName').val();
+    var date = $('#eventDate').val();
+    var time = $('#eventTime').val();
+    var desc = $('#eventBody').val();
+    events.child(date + "-" + time + "-" + title).set({
+        title: title,
+        date: date,
+        time: time,
+        description: desc,
+    });
+    readFromFire();
+    $('#eventName').val("");
+    $('#eventDate').val("");
+    $('#eventTime').val("");
+    $('#eventBody').val("");
 }
 
-function readFromFire(){
-	
-	var query = events.orderByValue();
-	query.once("value")
-	  .then(function(snapshot) {
-	  	$('#todo-container').html("");
-	    snapshot.forEach(function(child) {
-			var title = child.child("title").val();
-			var date = child.child("date").val();
-			var time = child.child("time").val();
-			var description = child.child("description").val();
-			$('#todo-container').append(
-				$('<span/>').attr('class','todo').append(
-					$('<span/>').attr('class','eventHead').html(title)
-				).append(
-					$('<span/>').attr('class','eventDate').html(date)
-				).append(
-					$('<span/>').attr('class','eventTime').html(time)
-				).append(
-					$('<span/>').attr('class','eventBody').html("- "+description)
-				).append(
-					$('<span/>').attr('class','delete').click(deleteEvent).html('×')
-				)
-			);
-	    });
-	});
+function readFromFire() {
+
+    var query = events.orderByValue();
+    query.once("value")
+        .then(function(snapshot) {
+            $('#todo-container').html("");
+            snapshot.forEach(function(child) {
+                var title = child.child("title").val();
+                var date = child.child("date").val();
+                var time = child.child("time").val();
+                var description = child.child("description").val();
+                $('#todo-container').append(
+                    $('<span/>').attr('class', 'todo').append(
+                        $('<span/>').attr('class', 'eventHead').html(title)
+                    ).append(
+                        $('<span/>').attr('class', 'eventDate').html(date)
+                    ).append(
+                        $('<span/>').attr('class', 'eventTime').html(time)
+                    ).append(
+                        $('<span/>').attr('class', 'eventBody').html("- " + description)
+                    ).append(
+                        $('<span/>').attr('class', 'delete').click(deleteEvent).html('×')
+                    )
+                );
+            });
+        });
     // $('.delete').click(deleteEvent);
-	hideModal();
+    hideModal();
 }
 
-var deleteEvent = function(e){
-	var box = $(e.target);
-	var key = box.parent().find('.eventDate').html()+"-"+box.parent().find('.eventTime').html()+"-"+box.parent().find('.eventHead').html();
-	events.child(key).remove();
-	readFromFire();
+var deleteEvent = function(e) {
+    var box = $(e.target);
+    var key = box.parent().find('.eventDate').html() + "-" + box.parent().find('.eventTime').html() + "-" + box.parent().find('.eventHead').html();
+    events.child(key).remove();
+    readFromFire();
 }.bind(this);
--- js/weather.js
+++ js/weather.js
@@ -1,58 +1,68 @@
-$(function(){
+$(function() {
+    /* Adjust these two urls to suit the location of your choice
+    Easiest way is to just adjust the coordinates in the urls (43.9713,-79.2468), etc
+    darksky.net
+    
+    DarkSky was also bought out by Apple recently, so their api will stop working come 2021.
+    Hopefully a suitable alternative appears before then.*/
+    var URL1 = "https://api.darksky.net/forecast/85ee59f33b71d050c766551ca2d2f139/43.9713,-79.2468?callback=?&units=ca";
+    var URL2 = "https://api.darksky.net/forecast/85ee59f33b71d050c766551ca2d2f139/43.654,-79.3872?callback=?&units=ca";
+    //Set the names of your locations here. Match name1 with URL1, and so on.
+    var name1 = "Stouffville";
+    var name2 = "Toronto";
 
-  //Whitehorse var stoURL = "https://api.darksky.net/forecast/e7ea57ca52da5ae872c7c0c343f312be/60.7216,-135.0549?callback=?&units=ca";
-	var stoURL = "https://api.darksky.net/forecast/e7ea57ca52da5ae872c7c0c343f312be/43.9713,-79.2468?callback=?&units=ca";
-  //Winnipeg var torURL = "https://api.darksky.net/forecast/e7ea57ca52da5ae872c7c0c343f312be/49.884,-97.1686?callback=?&units=ca";
-	var torURL = "https://api.darksky.net/forecast/e7ea57ca52da5ae872c7c0c343f312be/43.654,-79.3872?callback=?&units=ca";
+    //openweather api ac44343b90759cfe705813ff3a614fa5 85ee59f33b71d050c766551ca2d2f139 http://api.openweathermap.org/data/2.5/weather?q=Stouffville&appid=ac44343b90759cfe705813ff3a614fa5
 
-	$.getJSON(stoURL, function(data) {
-		var icons = new Skycons({"color": $('.button').css('color')});
-		icons.set('weathericon-sto', data.currently.icon);
-		icons.play();
-		$('#stouffville #temperature').html(data.currently.temperature+"°C");
-		$('#stouffville #conditions').html(data.currently.summary);
-		if(data.currently.apparentTemperature){
-			$('#stouffville #feels-like').html("<b>Feels Like: </b>"+data.currently.apparentTemperature.toFixed(1)+"°C");
-		}
-		else{
-			$('#stouffville #feels-like').remove();
-		}
-    if (!data.alerts) 
-      $("#sto-alerts").remove();
-    else{
-      for (var i = data.alerts.length - 1; i >= 0; i--) {
-        $("#alert-body-sto").append($("<span/>").addClass("alert-title").html(data.alerts[i].title)).append($("<br/>")).append($("<br/>"));
-        $("#alert-body-sto").append($("<span/>").addClass("alert-body").html(data.alerts[i].description)).append($("<br/>")).append($("<br/>"));
-        if (i!=0) {$("#alert-body-sto").append($("<hr>"))}
-      }
-    }
-		$('#stouffville #high').html("<b>H/L: </b>"+data.daily.data[0].temperatureHigh.toFixed(1)+"°C/"+data.daily.data[0].temperatureLow.toFixed(1)+"°C");
-		$('#stouffville #wind').html("<b>Wind: </b>"+data.currently.windSpeed+"km/h <b class='arrow'>"+getDirection(data.currently.windBearing))+"</b>";
-	});
-	$.getJSON(torURL, function(data) {
-		var icons = new Skycons({"color": $('.button').css('color')});
-		icons.set('weathericon-tor', data.currently.icon);
-		icons.play();
-		$('#toronto #temperature').html(data.currently.temperature+"°C");
-		$('#toronto #conditions').html(data.currently.summary);
-		if(data.currently.apparentTemperature){
-			$('#toronto #feels-like').html("<b>Feels Like: </b>"+data.currently.apparentTemperature.toFixed(1)+"°C");
-		}
-		else{
-			$('#toronto #feels-like').remove();
-		}
-    if (!data.alerts) 
-      $("#tor-alerts").remove();
-    else{
-      for (var i = data.alerts.length - 1; i >= 0; i--) {
-        $("#alert-body-tor").append($("<span/>").addClass("alert-title").html(data.alerts[i].title)).append($("<br/>")).append($("<br/>"));
-        $("#alert-body-tor").append($("<span/>").addClass("alert-body").html(data.alerts[i].description)).append($("<br/>")).append($("<br/>"));
-        if (i!=0) {$("#alert-body-tor").append($("<hr>"))}
-      }
-    }
-		$('#toronto #high').html("<b>H/L: </b>"+data.daily.data[0].temperatureHigh.toFixed(1)+"°C/"+data.daily.data[0].temperatureLow.toFixed(1)+"°C");
-		$('#toronto #wind').html("<b>Wind: </b>"+data.currently.windSpeed+"km/h <b class='arrow'>"+getDirection(data.currently.windBearing))+"</b>";
-	});
+    //Set names
+    $("#weathername-1").html(name1);
+    $("#weathername-2").html(name2);
+
+    $.getJSON(URL1, function(data) {
+        var icons = new Skycons({ "color": $('.button').css('color') });
+        icons.set('weathericon-1', data.currently.icon);
+        icons.play();
+        $('#weather-1 #temperature').html(data.currently.temperature + "°C");
+        $('#weather-1 #conditions').html(data.currently.summary);
+        if (data.currently.apparentTemperature) {
+            $('#weather-1 #feels-like').html("<b>Feels Like: </b>" + data.currently.apparentTemperature.toFixed(1) + "°C");
+        } else {
+            $('#weather-1 #feels-like').remove();
+        }
+        if (!data.alerts)
+            $("#alerts-1").remove();
+        else {
+            for (var i = data.alerts.length - 1; i >= 0; i--) {
+                $("#alert-body-1").append($("<span/>").addClass("alert-title").html(data.alerts[i].title)).append($("<br/>")).append($("<br/>"));
+                $("#alert-body-1").append($("<span/>").addClass("alert-body").html(data.alerts[i].description)).append($("<br/>")).append($("<br/>"));
+                if (i != 0) { $("#alert-body-1").append($("<hr>")) }
+            }
+        }
+        $('#weather-1 #high').html("<b>H/L: </b>" + data.daily.data[0].temperatureHigh.toFixed(1) + "°C/" + data.daily.data[0].temperatureLow.toFixed(1) + "°C");
+        $('#weather-1 #wind').html("<b>Wind: </b>" + data.currently.windSpeed + "km/h <b class='arrow'>" + getDirection(data.currently.windBearing)) + "</b>";
+    });
+    $.getJSON(URL2, function(data) {
+        var icons = new Skycons({ "color": $('.button').css('color') });
+        icons.set('weathericon-2', data.currently.icon);
+        icons.play();
+        $('#weather-2 #temperature').html(data.currently.temperature + "°C");
+        $('#weather-2 #conditions').html(data.currently.summary);
+        if (data.currently.apparentTemperature) {
+            $('#weather-2 #feels-like').html("<b>Feels Like: </b>" + data.currently.apparentTemperature.toFixed(1) + "°C");
+        } else {
+            $('#weather-2 #feels-like').remove();
+        }
+        if (!data.alerts)
+            $("#alerts-2").remove();
+        else {
+            for (var i = data.alerts.length - 1; i >= 0; i--) {
+                $("#alert-body-2").append($("<span/>").addClass("alert-title").html(data.alerts[i].title)).append($("<br/>")).append($("<br/>"));
+                $("#alert-body-2").append($("<span/>").addClass("alert-body").html(data.alerts[i].description)).append($("<br/>")).append($("<br/>"));
+                if (i != 0) { $("#alert-body-tor").append($("<hr>")) }
+            }
+        }
+        $('#weather-2 #high').html("<b>H/L: </b>" + data.daily.data[0].temperatureHigh.toFixed(1) + "°C/" + data.daily.data[0].temperatureLow.toFixed(1) + "°C");
+        $('#weather-2 #wind').html("<b>Wind: </b>" + data.currently.windSpeed + "km/h <b class='arrow'>" + getDirection(data.currently.windBearing)) + "</b>";
+    });
 });
 
 var getJSON = function(url, callback) {
@@ -60,41 +70,47 @@ var getJSON = function(url, callback) {
     xhr.open('GET', url, true);
     xhr.responseType = 'json';
     xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
-      }
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
     };
     xhr.send();
 };
 
 
-function getDirection(bearing)
-{
-  var directions = [
-  	[0, 22.5],
-  	[22.5, 67.5],
-  	[67.5, 112.5],
-  	[112.5, 157.5],
-  	[157.5, 202.5],
-  	[202.5, 247.5],
-  	[247.5, 292.5],
-  	[292.5, 337.5],
-  	[337.5, 360]
-  ];
+function getDirection(bearing) {
+    var directions = [
+        [0, 22.5],
+        [22.5, 67.5],
+        [67.5, 112.5],
+        [112.5, 157.5],
+        [157.5, 202.5],
+        [202.5, 247.5],
+        [247.5, 292.5],
+        [292.5, 337.5],
+        [337.5, 360]
+    ];
 
-  var arrows = ["↓","↙","←","↖","↑","↗","→","↘","↓"];
-  var x = 0;
-  var arrow = "";
-  directions.forEach(function(dir){
-  	if (bearing>=dir[0] && bearing<dir[1]) {
-  		// console.log(arrows[x]);
-  		arrow =  arrows[x];
-  	}
-  	x++;
-  });
-  return arrow;
+    var arrows = ["↓", "↙", "←", "↖", "↑", "↗", "→", "↘", "↓"];
+    var x = 0;
+    var arrow = "";
+    directions.forEach(function(dir) {
+        if (bearing >= dir[0] && bearing < dir[1]) {
+            // console.log(arrows[x]);
+            arrow = arrows[x];
+        }
+        x++;
+    });
+    return arrow;
 }
 
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
+}
