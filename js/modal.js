33c7c1f87af8c3b3c3bf7ac04dfc22f76d2b5354
Owen Bowden (the404) <47672957+the404devs@users.noreply.github.com>
2025-08-27 16:26:29 -0400

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -2,28 +2,13 @@
 let currentModal = null;
 
 function showModal(id) {
-    // Hide everything that needs to be hidden
-    $('#main').style.filter = 'blur(25px)';
-    $('#header').style.filter = 'blur(25px)';
-    $('#header').style.backdropFilter = 'none';
-    $('#xkcd-zone').style.backdropFilter = 'none';
-    $('#xkcd-zone').style.filter = 'blur(25px)';
-    
     // Show the modal
     const modal = $(id);
-    modal.style.filter = 'none'
-    modal.style.backdropFilter = 'blur(10px)';
     fadeIn(modal);
     currentModal = id;
 }
 
 function hideModal() {
-    // Unhide everything that needs to be unhidden
-    $('#main').style.filter = 'none';
-    $('#header').style.filter = 'none';
-    $('#header').style.backdropFilter = 'blur(10px)';
-    $('#xkcd-zone').style.backdropFilter = 'blur(10px)';
-    $('#xkcd-zone').style.filter = 'none';
     // Unshow the modals. All of them.
     $$('.modal').forEach(modal => {
        fadeOut(modal);
