54b98faeb91bc2d479fadc33180b2b75271fa593
Owen Bowden (the404) <47672957+the404devs@users.noreply.github.com>
2025-08-27 15:45:20 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+
+
+function fadeIn(elem, duration = 400, display = 'block') {
+  elem.style.opacity = 0;
+  elem.style.display = 'block';
+
+  let start = null;
+  function animate(timestamp) {
+    if (!start) start = timestamp;
+    let progress = (timestamp - start) / duration;
+    if (progress > 1) progress = 1;
+
+    elem.style.opacity = progress;
+
+    if (progress < 1) {
+      requestAnimationFrame(animate);
+    }
+  }
+  requestAnimationFrame(animate);
+}
